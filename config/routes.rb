7db304c3280c931a163d10903c5575f97ba9c780
nizhikebinesi <nizhikebinesi@rambler.ru>
2016-11-18 03:59:37 -0800

    Updates for heroku deployment

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  root 'myapp#index'
+  root 'drinks#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
